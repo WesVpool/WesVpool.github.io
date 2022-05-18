8ae6c607a4609811fab9e3f6514a72fab4c7195f
WesVpool <westonvanderpool@gmail.com>
2022-05-18 10:08:25 -0500

    redesign. added projects.html

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const tag = document.getElementById('tag1')
-var time = 100;
-const getTime = ()=>{
-    time = time - 1
-tag.innerHTML = "Goodbye Turtles: Party Wagon leaves in " + time + 'secs';
-}
-setInterval(getTime, 1000);
-alert("LEAVE NOW IF YOU ARE NOT A FAN OF THE TURTLES")
-alert("WELCOME")
+//const tag = document.getElementById('tag1')
+//var time = 5;
+//const getTime = ()=>{
+//    if(time > 0){
+//    time = time - 1}
+//tag.innerHTML = "Goodbye Turtles: Party Wagon leaves in " + time + 'secs';
+//}
+//setInterval(getTime, 1000);
+//alert("LEAVE NOW IF YOU ARE NOT A FAN OF THE TURTLES")
+//alert("WELCOME")
