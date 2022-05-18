7ed424a8699c14b4512576cfc356e9bde9d98a24
WesVpool <westonvanderpool@gmail.com>
2022-05-18 12:21:08 -0500

    additional styling. added footer

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-//const tag = document.getElementById('tag1')
-//var time = 5;
-//const getTime = ()=>{
-//    if(time > 0){
-//    time = time - 1}
-//tag.innerHTML = "Goodbye Turtles: Party Wagon leaves in " + time + 'secs';
-//}
-//setInterval(getTime, 1000);
-//alert("LEAVE NOW IF YOU ARE NOT A FAN OF THE TURTLES")
-//alert("WELCOME")
+alert("WELCOME")
